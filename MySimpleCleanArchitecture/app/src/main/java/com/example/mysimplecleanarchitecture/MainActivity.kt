707cc520d4a3615324df26cb591993628f2e5e5f
Dimas Fahrul <dfpa.05@gmail.com>
2025-02-11 23:57:23 +0700

    init: project

---
+++ MySimpleCleanArchitecture/app/src/main/java/com/example/mysimplecleanarchitecture/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.mysimplecleanarchitecture
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import com.example.mysimplecleanarchitecture.databinding.ActivityMainBinding
+import com.example.mysimplecleanarchitecture.presentation.MainViewModel
+import com.example.mysimplecleanarchitecture.presentation.MainViewModelFactory
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val factory = MainViewModelFactory.getInstance()
+        val viewModel = ViewModelProvider(this, factory)[MainViewModel::class.java]
+
+        viewModel.setName("Dimas")
+        viewModel.message.observe(this) {
+            binding.tvWelcome.text = it.welcomeMessage
+        }
+    }
+}
