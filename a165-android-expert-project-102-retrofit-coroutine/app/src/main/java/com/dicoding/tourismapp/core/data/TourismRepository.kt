3229e8d5e207edf2a6a1a8d6b577715a6db89b3c
Dimas Fahrul <dfpa.05@gmail.com>
2025-02-23 22:09:16 +0700

    feat: coroutine study case

--- a165-android-expert-project-102-retrofit-coroutine/app/src/main/java/com/dicoding/tourismapp/core/data/TourismRepository.kt
+++ a165-android-expert-project-102-retrofit-coroutine/app/src/main/java/com/dicoding/tourismapp/core/data/TourismRepository.kt
@@ -1,7 +1,6 @@
 package com.dicoding.tourismapp.core.data
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.map
+
 import com.dicoding.tourismapp.core.data.source.local.LocalDataSource
 import com.dicoding.tourismapp.core.data.source.remote.RemoteDataSource
 import com.dicoding.tourismapp.core.data.source.remote.network.ApiResponse
@@ -10,6 +9,8 @@ import com.dicoding.tourismapp.core.domain.model.Tourism
 import com.dicoding.tourismapp.core.domain.repository.ITourismRepository
 import com.dicoding.tourismapp.core.utils.AppExecutors
 import com.dicoding.tourismapp.core.utils.DataMapper
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
 class TourismRepository private constructor(
     private val remoteDataSource: RemoteDataSource,
@@ -31,9 +32,9 @@ class TourismRepository private constructor(
             }
     }
 
-    override fun getAllTourism(): LiveData<Resource<List<Tourism>>> =
-        object : NetworkBoundResource<List<Tourism>, List<TourismResponse>>(appExecutors) {
-            override fun loadFromDB(): LiveData<List<Tourism>> {
+    override fun getAllTourism(): Flow<Resource<List<Tourism>>> =
+        object : NetworkBoundResource<List<Tourism>, List<TourismResponse>>() {
+            override fun loadFromDB(): Flow<List<Tourism>> {
                 return localDataSource.getAllTourism().map {
                     DataMapper.mapEntitiesToDomain(it)
                 }
@@ -43,16 +44,16 @@ class TourismRepository private constructor(
                 data.isNullOrEmpty() // mengambil data dari internet hanya jika data di database kosong
 //                 true // ganti dengan true jika ingin selalu mengambil data dari internet
 
-            override fun createCall(): LiveData<ApiResponse<List<TourismResponse>>> =
+            override suspend fun createCall(): Flow<ApiResponse<List<TourismResponse>>> =
                 remoteDataSource.getAllTourism()
 
-            override fun saveCallResult(data: List<TourismResponse>) {
+            override suspend fun saveCallResult(data: List<TourismResponse>) {
                 val tourismList = DataMapper.mapResponsesToEntities(data)
                 localDataSource.insertTourism(tourismList)
             }
-        }.asLiveData()
+        }.asFlow()
 
-    override fun getFavoriteTourism(): LiveData<List<Tourism>> {
+    override fun getFavoriteTourism(): Flow<List<Tourism>> {
         return localDataSource.getFavoriteTourism().map {
            DataMapper.mapEntitiesToDomain(it)
         }
